31634484693282a9f971a50a03f80501ce32b32f
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-08-12 23:14:37 +0500

    add comment in file SelectionSort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,22 +1,22 @@
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] list = {8, 1, 2, 4, 9, 3, 7, 0, 5, 6};
+        int[] list = {8, 1, 2, 4, 9, 3, 7, 0, 5, 6};    // задаём не сортированный массив
 
-        runSort(list);
+        runSort(list);      // вызываем функцию сортировки
 
-        for (int i:
+        for (int i:         // выводим сортированный массив
              list) {
             System.out.print(list[i] + " ");
         }
     }
 
     public static void runSort(int[] list) {
-        int temp;
+        int temp;       // создаём переменную темп
 
-        for (int i = 0; i < list.length; i++) {
-            for (int j = i + 1; j < list.length; j++) {
-                if (list[i] < list[j]) {
-                    temp = list[i];
+        for (int i = 0; i < list.length; i++) {     // сравниваем первое значение с остальными, после +1
+            for (int j = i + 1; j < list.length; j++) {     // пошагово сравниваем весь массив с первым значением
+                if (list[i] < list[j]) {    // если первое значение меньше, то меняем местами
+                    temp = list[i];         // используя переменную темп
                     list[i] = list[j];
                     list[j] = temp;
                 }
